f24606a891ae7ecfeb04c603dfbbf798ae0d262d
JonasHri <jonasharriehausen@gmail.com>
2025-01-09 11:01:03 +0100

    removed redundant lines

--- jonas/src/bin/q09_3.rs
+++ jonas/src/bin/q09_3.rs
@@ -10,21 +10,13 @@ fn main() {
         .collect::<Vec<usize>>();
 
     let mut needed_stamps =
-        vec![-1].repeat(brightnesses.iter().max().unwrap().to_owned() + 1 as usize);
+        vec![0].repeat(brightnesses.iter().max().unwrap().to_owned() + 1 as usize);
 
     let stamps = vec![
         1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101,
     ];
 
-    for stamp in &stamps {
-        needed_stamps[*stamp] = 1;
-    }
-
-    for i in 2..needed_stamps.len() {
-        if needed_stamps[i] >= 1 {
-            continue;
-        }
-
+    for i in 1..needed_stamps.len() {
         let valid_stamps = stamps.iter().filter(|x| **x <= i).collect::<Vec<&usize>>();
         needed_stamps[i] = valid_stamps
             .iter()
@@ -33,25 +25,12 @@ fn main() {
             .unwrap() as i32;
     }
 
-    println!(
-        "{}",
-        brightnesses.iter().map(|x| needed_stamps[*x]).sum::<i32>()
-    );
-
     let mut total_stamps = 0;
     for br in brightnesses {
-        let mut combo_stamps = 0;
-        if br % 2 == 0 {
-            combo_stamps = (0..50)
-                .map(|x| needed_stamps[br / 2 - x] + needed_stamps[br / 2 + x])
-                .min()
-                .unwrap();
-        } else {
-            combo_stamps = (0..49)
-                .map(|x| needed_stamps[br / 2 - x] + needed_stamps[br / 2 + x + 1])
-                .min()
-                .unwrap();
-        }
+        let combo_stamps = (0..50 - br % 2)
+            .map(|x| needed_stamps[br / 2 - x] + needed_stamps[br / 2 + x + br % 2])
+            .min()
+            .unwrap();
         total_stamps += combo_stamps;
     }
 
