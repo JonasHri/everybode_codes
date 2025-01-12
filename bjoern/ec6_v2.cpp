e0516ebc6552e6f134567313c28852af19cf1031
Björn <bjoern.97@web.de>
2025-01-12 21:20:25 +0100

    v2 of ec6 running, but wrong result. Must read instructions more carfully....

--- bjoern/ec6_v2.cpp
+++ bjoern/ec6_v2.cpp
@@ -4,10 +4,12 @@
 #include <fstream>
 #include <unordered_map>
 #include <array>
+#include <optional>
 
+std::optional<std::vector<std::string>> findUniq(const std::vector<std::vector<std::string>> paths);
 
 int main(){
-    std::unordered_map<std::string, std::vector<std::string>> input;
+    std::unordered_map<std::string, std::string> input;
 
     std::string line;
     std::fstream file("everybody_codes_e2024_q06_p1.txt");
@@ -17,21 +19,11 @@ int main(){
     while(std::getline(file, line)){
         size_t sep= line.find(':');
         std::string father = line.substr(0, sep);
-        std::vector<std::string> children;
         std::string kids = line.substr(sep+1);
-        std::string kid; 
-        sep= kids.find(',');
-        while(sep!=std::string::npos){
-            //std::cout << kids << sep <<  std::endl;  
-            kid= kids.substr(0, sep);
-            kids= kids.substr(sep+1);
-            sep= kids.find(',');
-            children.push_back(kid);
-        }
-        children.push_back(kids); 
-        input.insert({father, children});
+        input.insert({father, kids});
     }
 
+    /*
     for(const std::pair<const std::string, std::vector<std::string>>& p: input){
         std::cout <<p.first <<": ";
         for(const auto& s: p.second){
@@ -39,10 +31,68 @@ int main(){
         } 
         std::cout << std::endl; 
     }
-
-    
-
-
+    */
+    std::vector<std::string> path; 
+    std::vector<std::vector<std::string>> paths; 
+    std::pair<std::string, std::string> curr; 
+    for(const std::pair<const std::string, std::string>& node: input){
+        if(node.second.contains('@')){  //gibts in c++23 :)
+            path.clear();
+            path.push_back(node.first);
+            curr= node; 
+            while(curr.first!="RR"){
+                for(const std::pair<const std::string, std::string>& otherNode: input){
+                    if(otherNode.second.contains(curr.first)){
+                        curr=otherNode; 
+                        path.push_back(curr.first);
+                    }
+                }
+            }
+            paths.push_back(path);
+        }
+    }
+    if(auto erg=findUniq(paths)){
+        std::cout << "ergebnis ist: ";
+        for(int i= erg->size() -1; i>=0; i--){
+            //std::cerr << i; 
+          //  std::cout <<i << ": "; 
+            std::cout << (*erg)[i]<< " ";
+        }
+        //std::cout << (*erg)[0];
+        /*std::cout << "\n und das 2." << std::endl;
+        for(const auto l: *erg){
+            std::cout << l<<" ";
+        } */
+        std::cout <<std::endl; 
+    } else{
+        std::cout << "no erg" <<std::endl;
+    }
 
     return 0; 
+}
+
+std::optional<std::vector<std::string>> findUniq(const std::vector<std::vector<std::string>> paths){
+    int heightA=0;
+    int heightB=0; 
+    int currPos=0;
+    for(std::vector<std::string> p:paths){
+        int tmp= p.size();
+        if(heightA==0){
+            heightA=tmp;
+        }else if(heightB==0){
+            heightB=tmp;
+        } else{
+            if(tmp==heightA && heightA==heightB){
+                continue; 
+            } else if(tmp==heightA && heightA!=heightB){
+                return paths[1];
+            } else if(tmp==heightB && heightA != heightB){
+                return paths[0];
+            } else if(heightA == heightB && tmp!=heightA){
+                return paths[currPos];
+            }
+        }
+        currPos++; 
+    }
+    return std::nullopt; 
 }
