37f75d569686a2caf9251bffbeff738b97ccb942
JonasHri <jonasharriehausen@gmail.com>
2025-01-17 07:10:40 +0100

    changed quest 3 part 3 solutions

--- jonas/src/bin/q03_3_pad.rs
+++ jonas/src/bin/q03_3_pad.rs
@@ -1,5 +1,5 @@
-use std::cmp::{max, min};
 use std::fs;
+
 fn main() {
     let field: Vec<Vec<char>> = fs::read_to_string("./inputs/everybody_codes_e2024_q03_p3.txt")
         .unwrap()
@@ -7,24 +7,26 @@ fn main() {
         .map(|line| line.chars().collect())
         .collect();
 
-    let mut depths = vec![vec![0; field[0].len()]; field.len()];
+    let mut big_field: Vec<Vec<char>> = vec![vec!['.'; field[0].len() + 2]; field.len() + 2];
+    for (i, col) in field.iter().enumerate() {
+        for (j, val) in col.iter().enumerate() {
+            big_field[i + 1][j + 1] = *val;
+        }
+    }
+
+    let mut depths = vec![vec![0; big_field[0].len()]; big_field.len()];
     for _ in 0..100 {
-        for i in 0..field.len() {
-            for j in 0..field[i].len() {
-                if field[i][j] != '#' {
+        for i in 1..big_field.len() - 1 {
+            for j in 1..big_field[i].len() - 1 {
+                if big_field[i][j] != '#' {
                     continue;
                 }
                 let mut adj: Vec<i32> = Vec::new();
 
-                for cols in &depths[max(i as i32 - 1, 0) as usize..=min(i + 1, depths.len() - 1)] {
-                    for val in
-                        &cols[max(j as i32 - 1, 0) as usize..=min(j + 1, depths[0].len() - 1)]
-                    {
+                for col in &depths[i - 1..=i + 1] {
+                    for val in &col[j - 1..=j + 1] {
                         adj.push(*val);
                     }
-                }
-                if adj.len() != 9 {
-                    adj.push(0);
                 }
                 depths[i][j] = adj.iter().min().unwrap() + 1;
             }
