d75d5e557477b98bbccf49186142c73f01158a30
Björn <bjoern.97@web.de>
2025-03-18 17:33:38 +0100

    testcase work for ec17 p3. idk why the real challange doesn't

--- bjoern/ec17.cpp
+++ bjoern/ec17.cpp
@@ -6,6 +6,8 @@
 #include <unordered_set>
 #include <cmath>
 #include <queue>
+#include <optional>
+#include <algorithm>
 
 struct point{
     int x, y;
@@ -92,22 +94,116 @@ int MST(std::unordered_set<point, pointHash>& stars){
     return totalWeight; 
 }
 
+std::vector<std::vector<point>> findConstellation(std::vector<point>& points){
+    std::vector<std::vector<point>> constellations;
+    for(point& p: points){
+        // if(p.visited){
+        //     continue;
+        // }
+        std::vector<point> constellation; 
+        for(const point& p2: points){
+            if(p2.visited){
+                continue;
+            }
+            if(p.manhattenAbstand(p2)<6){
+                constellation.push_back(p2);
+            }
+        }
+        constellations.push_back(constellation); 
+        // p.visited=true; 
+    }
+    std::sort(constellations.begin(), constellations.end(), [&] (std::vector<point>& a, std::vector<point>& b){return a.size() > b.size(); }); 
+    std::vector<std::vector<point>> uniqConstellations; 
+    std::unordered_set<point, pointHash> used; 
+    std::cout << "es gibt "<<constellations.size() << " Konstellationen "<< std::endl; 
+    int i=1; 
+    for(auto& constellation: constellations){
+        std::cout << "Die "<<i++<<".-te Konstellation hat "<<constellation.size() <<" Sterne "<<std::endl; 
+        bool uniq =true; 
+        for(point& p: constellation){
+            if(used.contains(p)){
+                uniq = false; 
+                break; 
+            }
+            used.insert(p); 
+        }
+        if(uniq){
+            uniqConstellations.push_back(constellation); 
+        }
+    }
+    std::cout << "Es gibt davon "<<uniqConstellations.size() <<" einzigartige Konstellationen "<<std::endl; 
+    return uniqConstellations; 
+}
 
+int brilliantMST(std::vector<point>& points, int start=0){
+    std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>, std::greater<>> q;
+    for(point& p: points){
+        p.visited=false; 
+    }
+    q.push({0,start});
+    int starsInconstellation = 0; 
+    int totalWeight=0; 
+    while(!q.empty()){
+        auto [weight, u]= q.top();
+        q.pop();
+        if(points[u].visited){
+            continue;
+        }
+        totalWeight += weight;
+        points[u].visited=true; 
+        starsInconstellation++; 
+
+        for(int v=0; v<points.size(); v++){
+            if(!points[v].visited){
+                q.push({points[u].manhattenAbstand(points[v]), v});
+                // std::cout << "pushing: "<< points[v] <<std::endl; 
+            }
+        }
+    }
+    return totalWeight + starsInconstellation; 
+}
 
 int main(){
     std::vector<std::string> map;
     std::unordered_set<point, pointHash> stars;
     
-    std::fstream file("everybody_codes_e2024_q17_p2.txt");
+    std::fstream file("everybody_codes_e2024_q17_p3.txt");
     parseInput(file, map, stars);
     // int starcount=1; 
     // for(auto it=stars.begin(); it!= stars.end(); it++){
     //     std::cout << starcount++ << ": "<< *it << std::endl; 
     // }
+    std::vector<point> points;
+    for(point s: stars){
+        points.push_back(s);
+    }
 
-    int erg= MST(stars); 
+    int erg=0;
+    int l1{0}, l2{0}, l3{0};  
+    std::vector<std::vector<point>> constellations= findConstellation(points);
+    // for(auto& c: constellations){
+    //     std::cout << c.size() <<std::endl; 
+    // }
+    for(auto constellation: constellations){
+        int curr = brilliantMST(constellation); 
+        // std::cout << "From: " <<points[i]<<": "<<curr <<std::endl; 
+        if(curr>l1){
+            l3=l2;
+            l2=l1;
+            l1=curr;
+        } else if(curr>l2){
+            l3=l2;
+            l2=curr;
+        } else if(curr>l3){
+            l3=curr;
+        }
+    }
 
-    std::cout << erg + stars.size() <<std::endl; 
+    std::cout << l1 <<", " <<l2<< ", "<<l3 <<std::endl; 
+
+    erg= l1*l2*l3; 
+
+    std::cout << erg<<std::endl; 
 
     return 0; 
 }
